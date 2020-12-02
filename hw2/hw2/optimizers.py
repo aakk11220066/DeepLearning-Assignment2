8c2571df610334223d7b3031f31986cf75e2af38
nadav rubinstein <nadavru@campus.technion.ac.il>
2020-12-02 21:32:22 +0200

    .

---
+++ hw2/hw2/optimizers.py
@@ -0,0 +1,153 @@
+import abc
+import torch
+from torch import Tensor
+
+
+class Optimizer(abc.ABC):
+    """
+    Base class for optimizers.
+    """
+
+    def __init__(self, params):
+        """
+        :param params: A sequence of model parameters to optimize. Can be a
+        list of (param,grad) tuples as returned by the Blocks, or a list of
+        pytorch tensors in which case the grad will be taken from them.
+        """
+        assert isinstance(params, list) or isinstance(params, tuple)
+        self._params = params
+
+    @property
+    def params(self):
+        """
+        :return: A sequence of parameter tuples, each tuple containing
+        (param_data, param_grad). The data should be updated in-place
+        according to the grad.
+        """
+        returned_params = []
+        for x in self._params:
+            if isinstance(x, Tensor):
+                p = x.data
+                dp = x.grad.data if x.grad is not None else None
+                returned_params.append((p, dp))
+            elif isinstance(x, tuple) and len(x) == 2:
+                returned_params.append(x)
+            else:
+                raise TypeError(f"Unexpected parameter type for parameter {x}")
+
+        return returned_params
+
+    def zero_grad(self):
+        """
+        Sets the gradient of the optimized parameters to zero (in place).
+        """
+        for p, dp in self.params:
+            dp.zero_()
+
+    @abc.abstractmethod
+    def step(self):
+        """
+        Updates all the registered parameter values based on their gradients.
+        """
+        raise NotImplementedError()
+
+
+class VanillaSGD(Optimizer):
+    def __init__(self, params, learn_rate=1e-3, reg=0):
+        """
+        :param params: The model parameters to optimize
+        :param learn_rate: Learning rate
+        :param reg: L2 Regularization strength
+        """
+        super().__init__(params)
+        self.learn_rate = learn_rate
+        self.reg = reg
+
+    def step(self):
+        for p, dp in self.params:
+            if dp is None:
+                continue
+
+            # TODO: Implement the optimizer step.
+            #  Update the gradient according to regularization and then
+            #  update the parameters tensor.
+            # ====== YOUR CODE: ======
+            ndp = dp + self.reg * p
+            p -= self.learn_rate*ndp
+            # ========================
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, params, learn_rate=1e-3, reg=0, momentum=0.9):
+        """
+        :param params: The model parameters to optimize
+        :param learn_rate: Learning rate
+        :param reg: L2 Regularization strength
+        :param momentum: Momentum factor
+        """
+        super().__init__(params)
+        self.learn_rate = learn_rate
+        self.reg = reg
+        self.momentum = momentum
+
+        # TODO: Add your own initializations as needed.
+        # ====== YOUR CODE: ======
+        self.v_cache = {}
+        for i, (p, dp) in enumerate(self.params):
+            self.v_cache[i] = torch.zeros_like(p)
+        # ========================
+
+    def step(self):
+        for i, (p, dp) in enumerate(self.params):
+            if dp is None:
+                continue
+
+            # TODO: Implement the optimizer step.
+            # update the parameters tensor based on the velocity. Don't forget
+            # to include the regularization term.
+            # ====== YOUR CODE: ======
+            ndp = dp + self.reg * p
+            v = self.momentum * self.v_cache[i] - self.learn_rate * ndp
+            self.v_cache[i] = v
+            p += v
+            # ========================
+
+
+class RMSProp(Optimizer):
+    def __init__(self, params, learn_rate=1e-3, reg=0, decay=0.99, eps=1e-8):
+        """
+        :param params: The model parameters to optimize
+        :param learn_rate: Learning rate
+        :param reg: L2 Regularization strength
+        :param decay: Gradient exponential decay factor
+        :param eps: Constant to add to gradient sum for numerical stability
+        """
+        super().__init__(params)
+        self.learn_rate = learn_rate
+        self.reg = reg
+        self.decay = decay
+        self.eps = eps
+
+        # TODO: Add your own initializations as needed.
+        # ====== YOUR CODE: ======
+        self.r_cache = {}
+        for i, (p, dp) in enumerate(self.params):
+            self.r_cache[i] = 0
+        # ========================
+
+    def step(self):
+        for i, (p, dp) in enumerate(self.params):
+            if dp is None:
+                continue
+
+            # TODO: Implement the optimizer step.
+            # Create a per-parameter learning rate based on a decaying moving
+            # average of it's previous gradients. Use it to update the
+            # parameters tensor.
+            # ====== YOUR CODE: ======
+            ndp = dp + self.reg * p
+            value = (torch.norm(dp).item())**2
+            r = self.decay * self.r_cache[i] + (1-self.decay) * value
+            self.r_cache[i] = r
+            p -= (self.learn_rate/(r+self.eps)**0.5) * ndp
+            # ========================
